eefbb47d85221aadaf59ca0dfa32cb723d310bf4
Marcin Płóciennik <mplociennik@trans.eu>
2016-08-24 14:53:53 +0200

    update

--- get_config.py
+++ get_config.py
@@ -3,6 +3,10 @@
 import os
 import urllib2
 import ConfigParser
+import json
 
-response = urllib2.urlopen('https://raw.githubusercontent.com/mplociennik/testConfig/master/config.cfg')
-print response
+response = urllib2.urlopen('https://raw.githubusercontent.com/mplociennik/testConfig/master/config.json')
+data = json.load(response)
+
+for key in data['config']:
+    print '{0}:{1}'.format(key, data['config'][key])
